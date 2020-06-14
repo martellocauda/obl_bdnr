9f723055a56c846eeb88fe19c2dc6e62fb83eeb1
root <root@LaptopFranco.localdomain>
2020-06-14 20:40:10 -0300

    Rename venv, adding styles and js to move between funcionalities. Add redis and some redis code.

---
+++ github.py
@@ -0,0 +1,38 @@
+import requests, json, redis
+from flask import Flask,render_template, request
+from elasticsearch import Elasticsearch
+
+app = Flask(__name__)
+es = Elasticsearch([{'host': 'localhost', 'port': 9200}])
+r = redis.Redis(host='localhost', port=6379, db=0)
+
+def make_es_search(data_to_search):
+    response=es.search(index='source_code_data',
+        body={
+            'query': {
+                'match': {
+                    'sourceCode': data_to_search
+                }
+            }
+        }
+    )
+    #adjusting response
+    dumped_response = json.dumps(response)
+    jsonObject = json.loads(dumped_response)
+    jsonObject = jsonObject['hits']
+    return jsonObject
+
+
+@app.route('/', methods=['GET'])
+def index():
+        return render_template('index.html')
+
+
+@app.route('/search', methods=['POST'])
+def search():
+    ##ver si viene ordenada por score, sino, ordenar
+    search_result=make_es_search(request.form['to_search'])
+    return render_template('results.html', search_result=search_result, test="asd")
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)
---
+++ static/js/header.js
@@ -0,0 +1,33 @@
+$(function() {
+    $('.links').click(function() {
+	$('.active').attr('class', 'links');
+        $(this).attr('class', 'links active');
+    });
+});
+
+$(function() {
+    $('#feed_btn').click(function() {
+	$('#notimplementedcontainer').attr('class', 'container_not_active');
+	$('#searchcontainer').attr('class', 'container_not_active');
+	$('#searchescontainer').attr('class', 'container_not_active');
+        $('#feedscontainer').attr('class', 'container_active');
+    });
+});
+
+$(function() {
+    $('#search_btn').click(function() {
+	$('#notimplementedcontainer').attr('class', 'container_not_active');
+        $('#feedscontainer').attr('class', 'container_not_active');
+	$('#searchcontainer').attr('class', 'container_active');
+	$('#searchescontainer').attr('class', 'container_active');
+    });
+});
+
+$(function() {
+    $('.notimplemented_btn').click(function() {
+	$('#searchcontainer').attr('class', 'container_not_active');
+	$('#searchescontainer').attr('class', 'container_not_active');
+	$('#feedscontainer').attr('class', 'container_not_active');
+        $('#notimplementedcontainer').attr('class', 'container_active');
+    });
+});
